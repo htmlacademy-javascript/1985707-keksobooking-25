2ded1f70ca9a4aa662b407a0409315d83e724b7e
dannyskyyy <dannyskyyy@gmail.com>
2022-03-04 12:43:43 +0500

    Split the code into modules

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-function getRandomInt(min, max) {
-  if(min>=max || min<0 || max<0){
-    return 'Диапазон введен некорректно!';
-  }
+import { getArrayAds, SIMILAR_AD_COUNT } from './data.js';
 
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-getRandomInt(0,3);
-
-function getRandomFloat(min, max, n) {
-  if(min>=max || min<0 || max<0){
-    return 'Диапазон введен некорректно!';
-  }
-
-  return (Math.random() * (max - min) + min).toFixed(n);
-}
-
-getRandomFloat(0,3,1);
+getArrayAds(SIMILAR_AD_COUNT);
