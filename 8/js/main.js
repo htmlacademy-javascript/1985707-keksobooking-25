e1a54ff09babb42713181b9a4ddd540b3d85827a
Keks <keks@htmlacademy.ru>
2022-03-24 10:58:31 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/main.js
+++ 8/js/main.js
@@ -1,10 +1,9 @@
 import { getArrayOffers } from './data.js';
 import { insertOffers } from './ads-rendering.js';
 import { setFormValidation } from './ad-form.js';
-import {lockForm, unlockForm} from './form-initialization.js';
+import {lockForm} from './form-initialization.js';
 
 lockForm();
-unlockForm();
 insertOffers(getArrayOffers(1));
 setFormValidation();
 
